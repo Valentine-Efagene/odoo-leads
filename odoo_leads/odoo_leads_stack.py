a6e16dc148f261552ebc38b324cdef75318ec254
Valentine Efagene <efagenevalentine@gmail.com>
2025-08-29 06:39:04 +0100

    fix: env

--- odoo_leads/odoo_leads_stack.py
+++ odoo_leads/odoo_leads_stack.py
@@ -1,12 +1,14 @@
+import os
 from aws_cdk import (
     Duration,
     Stack,
     aws_sqs as sqs,
     aws_lambda as _lambda,
     aws_lambda_event_sources as lambda_event_sources,
-    BundlingOptions
+    BundlingOptions,
 )
 from constructs import Construct
+
 
 class OdooLeadsStack(Stack):
 
@@ -15,7 +17,8 @@ class OdooLeadsStack(Stack):
 
         # Create an SQS queue
         queue = sqs.Queue(
-            self, "OdooLeadsQueue",
+            self,
+            "OdooLeadsQueue",
             queue_name="OdooLeadsQueue",
             visibility_timeout=Duration.seconds(300),  # must be > lambda timeout
         )
@@ -29,10 +32,11 @@ class OdooLeadsStack(Stack):
                 bundling=BundlingOptions(
                     image=_lambda.Runtime.PYTHON_3_11.bundling_image,
                     command=[
-                        "bash", "-c",
+                        "bash",
+                        "-c",
                         "pip install --platform manylinux2014_x86_64 --only-binary=:all: "
-                        "-r requirements.txt -t /asset-output/python"
-                    ]
+                        "-r requirements.txt -t /asset-output/python",
+                    ],
                 ),
             ),
             compatible_runtimes=[_lambda.Runtime.PYTHON_3_11],
@@ -40,13 +44,20 @@ class OdooLeadsStack(Stack):
 
         # Lambda function that processes messages
         fn = _lambda.Function(
-            self, "OdooLeadLambda",
+            self,
+            "OdooLeadLambda",
             runtime=_lambda.Runtime.PYTHON_3_11,
             handler="worker.handler",  # file=worker.py, function=handler
             code=_lambda.Code.from_asset("lambda_src"),  # path to your code
             function_name="OdooLeadLambda",
             timeout=Duration.seconds(30),
             layers=[dependency_layer],
+            environment={
+                "ODOO_URL": os.getenv("ODOO_URL", ""),
+                "ODOO_DB": os.getenv("ODOO_DB", ""),
+                "ODOO_USERNAME": os.getenv("ODOO_USERNAME", ""),
+                "ODOO_PASSWORD": os.getenv("ODOO_PASSWORD", ""),
+            },
         )
 
         # Connect queue → lambda
