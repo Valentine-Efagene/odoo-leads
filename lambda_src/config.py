d7513acd0c3b5386df9e1b5e1209fc235c31a960
Valentine Efagene <efagenevalentine@gmail.com>
2025-08-29 07:09:59 +0100

    chore: cleanup

--- lambda_src/config.py
+++ lambda_src/config.py
@@ -3,8 +3,6 @@ import os
 
 
 class Settings(BaseSettings):
-    app_name: str = "QShelter Odoo-3cx API"
-    admin_email: str = "efagenevalentine@gmail.com"
     database_name: str = str(os.getenv("ODOO_DB"))
     password: str = str(os.getenv("ODOO_PASSWORD"))
     username: str = str(os.getenv("ODOO_USERNAME"))
